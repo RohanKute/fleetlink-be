a8232ba897c8a3afdc2ab7f8ec44a10476ee4e6d
Rohan Kute <rohanbk309@gmail.com>
2025-07-18 17:59:29 +0530

    CHANGE: app.ts-import-added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import vehicleRoutes from './routes/vehicleRoutes';
-
+import bookingRoutes from './routes/bookingRoutes'
 const app = express();
 app.use(express.json());
 
 app.use('/api/vehicles', vehicleRoutes);
+app.use('/api/bookings', bookingRoutes);
 
 export default app;
