b996d1b3898aa5eab43cb0e40835102af4e842ad
Rohan Kute <rohanbk309@gmail.com>
2025-07-20 11:49:10 +0530

    CHANGE: cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import vehicleRoutes from './routes/vehicleRoutes';
 import bookingRoutes from './routes/bookingRoutes'
+import cors from 'cors'
 const app = express();
+app.use(cors())
+
 app.use(express.json());
 
 app.use('/api/vehicles', vehicleRoutes);
